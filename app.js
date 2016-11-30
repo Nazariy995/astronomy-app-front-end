7f9f94dfa4fdc6e59e57a690bae3fd2420d7ddd1
Nazariy995 <nazariy1995@gmail.com>
2016-11-29 22:02:44 -0500

    CHNG: replaced ng-route with ui-router to enable states

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-var app = angular.module('hello', [ 'ngRoute' ]);
+var app = angular.module('hello', [ 'ui.router' ]);
 
-app.config(function($routeProvider, $httpProvider) {
+app.config(function($stateProvider, $httpProvider) {
+    var states = [
+        {
+            name:'login',
+            url:'/login',
+            templateUrl:'views/app/login/login.html',
+            controller: 'LoginCtrl',
+            controllerAs: 'ctrl'
+        },
+        {
+            name:'home',
+            url:'/home',
+            templateUrl:'views/app/home/home.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'ctrl'
+        },
+        {
+            name:'home.teacher',
+            url:'/teacher',
+            templateUrl:'views/teacher/home/home.html',
+            controller: 'Teacher.HomeCtrl',
+            controllerAs: 'TeacherHomeCtrl'
+        },
+        {
+            name:'home.student',
+            url:'/student',
+            templateUrl:'views/student/home/home.html',
+            controller: 'Student.HomeCtrl',
+            controllerAs: 'StudentHomeCtrl'
+        }
+    ]
 
-	$routeProvider.when('/', {
-		templateUrl : 'views/login/login.html',
-        controller: 'LoginCtrl',
-        controllerAs: 'ctrl'
-	}).when('/home',{
-        templateUrl : 'views/home/home.html',
-        controller: 'HomeCtrl',
-        controllerAs: 'ctrl',
-        css:'views/home/style.css'
+    states.forEach(function(state) {
+        $stateProvider.state(state);
     });
 
 	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
